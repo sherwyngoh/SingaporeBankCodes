6ad0b8c947fae280e430ce90f8415def26871772
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2015-04-22 12:25:08 +0800

    Apply DBS reference hash collection

--- spec/banks/dbs_spec.rb
+++ spec/banks/dbs_spec.rb
@@ -7,4 +7,6 @@ describe DBS do
   it {expect(subject.bank_code).to eq '7171'}
   it {expect(subject.branch_code).to eq '005'}
   it {expect(subject.resolved_number).to eq '0052312891'}
+  it {expect(subject.branch_name).to eq 'DBS Toa Payoh'}
+  
 end
